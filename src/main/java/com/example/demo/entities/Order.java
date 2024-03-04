3348d50fda06b5dc4b6eacc3fe01bfb4bd5257a7
Giovani <giovanifetzner01@gmail.com>
2024-03-04 16:16:29 -0300

    Implementação da entidade OrderItem

--- src/main/java/com/example/demo/entities/Order.java
+++ src/main/java/com/example/demo/entities/Order.java
@@ -2,11 +2,15 @@ package com.example.demo.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import com.example.demo.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,6 +18,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -35,6 +40,10 @@ public class Order implements Serializable{
 	@JoinColumn(name = "client_id")
 	private User client;
 
+	@Fetch(FetchMode.JOIN)
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Order() {
 	}
 
@@ -78,6 +87,10 @@ public class Order implements Serializable{
 			this.orderStatus = orderStatus.getCode();
 		}
 		
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
 	}
 
 	@Override
